30e1968937a5fca2b67e8a976ec313f1aa325f57
Moyna <faima.moyna@itchum.xyz>
2023-08-07 15:43:52 +0600

    true vs false

---
+++ script.js
@@ -0,0 +1,89 @@
+const questions = [
+    {
+        question: "Which is larget animal in the world?",
+        answers: [
+            { text: "Shark", correct: false},
+            { text: "Blue whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is larget animal in the world?",
+        answers: [
+            { text: "Vatican City", correct: false},
+            { text: "Bhutan", correct: false},
+            { text: "Nepal", correct: false},
+            { text: "Shri Lanka", correct: true},
+        ]  
+    },
+    {
+        question: "Which is larget animal in the world?",
+        answers: [
+            { text: "Kalahari", correct: false},
+            { text: "Gohi", correct: false},
+            { text: "Sanara", correct: true},
+            { text: "Antarctica", correct: false},
+        ]
+    },
+    {
+        question: "Which is larget animal in the world?",
+        answers: [
+            { text: "Asia", correct: true},
+            { text: "Australia", correct: false},
+            { text: "Arctic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.
+    question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+}
+startQuiz();
--- true-false.js
+++
@@ -1,8 +0,0 @@
-const age = -4;
-
-if(age){
-    console.log("condition is true");
-}
-else{
-    console.log("condition is false");
-}
